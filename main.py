1ec8ec2eeb687770466c3dd9aae44535291af2a9
Mohamed Afassi <80226544+mohanex@users.noreply.github.com>
2024-02-13 15:06:09 +0100

    restoring code

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ volume = cast(interface, POINTER(IAudioEndpointVolume))
 
 #the sound goes from -66 to 0db, with every 6db the sound doubles 
 #I need to find out a way to calculate the differencian equation between db and pc volume
-volume.SetMasterVolumeLevel(-38.0, None)
+volume.SetMasterVolumeLevel(-66.0, None)
